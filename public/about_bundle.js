abdf04353bd6dc5b3956253ddc4508b7b427cd3e
heegu0311 <heegu0311@gmail.com>
2022-03-26 17:43:15 +0900

    Output file setting with webpack config

--- public/about_bundle.js
+++ public/about_bundle.js
@@ -110,6 +110,16 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 
 /***/ }),
 
+/***/ "./source/about.js":
+/*!*************************!*\
+  !*** ./source/about.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _hello_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hello.js */ \"./source/hello.js\");\n/* harmony import */ var _world_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./world.js */ \"./source/world.js\");\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./style.css */ \"./source/style.css\");\n\n\n\ndocument.querySelector(\"#root\").innerHTML = _world_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"] + \" \" + _hello_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"];\nconsole.log(_style_css__WEBPACK_IMPORTED_MODULE_2__[\"default\"]); // css 로더가 js 파일 안으로 스타일 속성 내용이 JS 코드에 주입\n\n\n//# sourceURL=webpack://webpack/./source/about.js?");
+
+/***/ }),
+
 /***/ "./source/hello.js":
 /*!*************************!*\
   !*** ./source/hello.js ***!
@@ -117,16 +127,6 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst word = \"Hello\";\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (word);\n\n\n//# sourceURL=webpack://webpack/./source/hello.js?");
-
-/***/ }),
-
-/***/ "./source/index.js":
-/*!*************************!*\
-  !*** ./source/index.js ***!
-  \*************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _hello_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hello.js */ \"./source/hello.js\");\n/* harmony import */ var _world_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./world.js */ \"./source/world.js\");\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./style.css */ \"./source/style.css\");\n\n\n\ndocument.querySelector(\"#root\").innerHTML = _hello_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"] + \" \" + _world_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"];\nconsole.log(_style_css__WEBPACK_IMPORTED_MODULE_2__[\"default\"]); // css 로더가 js 파일 안으로 스타일 속성 내용이 JS 코드에 주입\n\n\n//# sourceURL=webpack://webpack/./source/index.js?");
 
 /***/ }),
 
@@ -212,7 +212,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./source/index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./source/about.js");
 /******/ 	
 /******/ })()
 ;
---
+++ source/about.js
@@ -0,0 +1,5 @@
+import hello_word from "./hello.js";
+import world_word from "./world.js";
+import css from "./style.css";
+document.querySelector("#root").innerHTML = world_word + " " + hello_word;
+console.log(css); // css 로더가 js 파일 안으로 스타일 속성 내용이 JS 코드에 주입
